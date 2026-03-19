1658e9b87c2fd7e9afbc23f145f98565a4c1938c
Paduma Induwara <induwarapaduma2002@gmail.com>
2026-03-19 20:40:38 +0530

    1.1

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ import CoreValues from "@/components/CoreValues";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import LatestEvents from "@/components/LatestEvents";
-import LiveEventModal from "@/components/LiveEventModal";
 
 export const metadata: Metadata = {
   title: "Home | St. Joseph's Girls' School Nugegoda",
@@ -127,11 +126,7 @@ export default function Home() {
       <section className="py-0 bg-gray-50" id="news" aria-label="Latest News and Upcoming Events">
         <NewsAndEvents />
       </section>
-
-      <section>
-        <LiveEventModal />
-      </section>
-
+      
       <section className="py-0" aria-label="Latest Events at St. Joseph's Girls' School">
         <LatestEvents />
       </section>
